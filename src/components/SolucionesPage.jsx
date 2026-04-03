efc80a9a8a8ac1151522a58b99074092f54d34af
Leo Orellana <orellanaleonel14@gmail.com>
2026-04-02 23:26:48 -0300

    fix: overflow-x hidden global and soluciones page

--- src/components/SolucionesPage.jsx
+++ src/components/SolucionesPage.jsx
@@ -2,16 +2,16 @@ import SolucionesHero from './SolucionesHero'
 import SolucionesDescripcion from './SolucionesDescripcion'
 import SolucionesGaleria from './SolucionesGaleria'
 import SolucionesAplicaciones from './SolucionesAplicaciones'
-import Contacto from './Contacto'
+import CTASection from './CTASection'
 
-export default function SmartLightsPage() {
+export default function SolucionesPage() {
   return (
-    <div className="w-full bg-[#030C40] shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+    <div className="w-full bg-[#030C40] overflow-hidden">
       <SolucionesHero />
       <SolucionesDescripcion />
       <SolucionesGaleria />
       <SolucionesAplicaciones />
-      <Contacto />
+      <CTASection />
     </div>
   )
 }
